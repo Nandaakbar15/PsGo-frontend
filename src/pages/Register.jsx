ae071071f53446a18d4b729649277b7b3f1801eb
Alvinda_akbar10 <alvindaakbar@gmail.com>
2025-05-22 17:31:29 +0700

    First Commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-import axios from 'axios'
+ import axios from 'axios'
 import React, { useState } from 'react'
 import { BtnRegister } from '../components/Button'
 import { Link, useNavigate } from 'react-router';
